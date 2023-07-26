ebacca05bf3c1cbe3368cd3b8d4a5a1f02182674
Dante Passalacqua <dante.l.pass@gmail.com>
2023-07-25 22:12:34 -0600

    Added header section

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+	return (
+		<header>
+			<h1 className="header-name">Dante Passalacqua</h1>
+			<div className="header-links">
+				<p>About</p>
+				<p>Projects</p>
+				<p>Contact</p>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
