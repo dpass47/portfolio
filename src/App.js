34847e210a6a6729ff5f9f9d9cbc5c98e04cd8a7
Dante Passalacqua <dante.l.pass@gmail.com>
2023-07-26 22:00:44 -0600

    Configured main content

--- src/App.js
+++ src/App.js
@@ -5,34 +5,32 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
-			<main>
+			<div className="main">
 				<div className="main-content">
 					<h2>
-						Hello! I'm Dante, a{' '}
+						Hello, I'm Dante, a{' '}
 						<span className="accent-text">developer</span> based in
 						Colorado.{' '}
 					</h2>
 					<p>
-						Pharmacy Technician turned into a development
-						enthusiast. I've loved everything to do with computers
-						since I was a kid. Once I found web development, I
-						absolutely fell in love.
-					</p>
-					<p>
 						I was a student at an online Frontend Developer course
 						where I spent 8 months learning the fundamentals of
-						frontend web development. I love building applications
-						that are user-friendly, simple and useful.
+						frontend web development. I love building simple,
+						beautiful user experiences.
 					</p>
 					<p>
 						I am currently looking for my first role as a developer.{' '}
+						<span className="main-contact">Contact me?</span>{' '}
 					</p>
 				</div>
 				<img
 					src={danteImage}
 					alt="Dante smiling in front of a brick wall"
 				/>
-			</main>
+			</div>
+			<div className="projects">
+				<h2 className="projects-header">Projects</h2>
+			</div>
 		</div>
 	);
 }
