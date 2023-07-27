61acc45e3c16a96449b46a2198d73d196ff21a2b
Dante Passalacqua <dante.l.pass@gmail.com>
2023-07-26 23:01:10 -0600

    Added data

---
+++ src/data.js
@@ -0,0 +1,69 @@
+const data = [
+	{
+		id: 0,
+		title: 'Celestial Dashboard',
+		summary:
+			'This project is based off the Momentum Dashboard chrome extension. I learned a tremendous amount in regards to working with APIs and building chrome extensions. The dashboard features weather, crypto price watch, quotes, and time widgets. All complete with a random space-themed background image taken from the Unsplash API.',
+		image: '',
+		languages: ['HTML5', 'CSS3', 'JavaScript'],
+		demoLink: 'https://dpass47.github.io/celestial-dashboard/',
+		sourceLink: 'https://github.com/dpass47/celestial-dashboard',
+	},
+	{
+		id: 1,
+		title: 'Tenzies Dice Game',
+		summary:
+			'This was my first app built with ReactJS, and I learned a great amount while building it. It is a dice game where users need to match the number of all ten die in order to win the game. The game tracks roll counts and time it takes to complete for high scores. When a user completes the game, confetti is shown to signify the win.',
+		image: '',
+		languages: ['HTML5', 'CSS3', 'JavaScript', 'ES6', 'ReactJS'],
+		demoLink: '',
+		sourceLink: '',
+	},
+	{
+		id: 2,
+		title: '',
+		summary: '',
+		image: '',
+		languages: [],
+		demoLink: '',
+		sourceLink: '',
+	},
+	{
+		id: 3,
+		title: '',
+		summary: '',
+		image: '',
+		languages: [],
+		demoLink: '',
+		sourceLink: '',
+	},
+	{
+		id: 4,
+		title: '',
+		summary: '',
+		image: '',
+		languages: [],
+		demoLink: '',
+		sourceLink: '',
+	},
+	{
+		id: 5,
+		title: '',
+		summary: '',
+		image: '',
+		languages: [],
+		demoLink: '',
+		sourceLink: '',
+	},
+	{
+		id: 6,
+		title: '',
+		summary: '',
+		image: '',
+		languages: [],
+		demoLink: '',
+		sourceLink: '',
+	},
+];
+
+export default data;
