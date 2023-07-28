6f73f2f7669c1ea72046d15a33736b80a4616987
Dante Passalacqua <dante.l.pass@gmail.com>
2023-07-27 19:00:16 -0600

    imported style file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,10 @@
+import '../styles/Header.css';
+
 function Header() {
 	return (
 		<header>
 			<h1 className="header-name">Dante Passalacqua</h1>
 			<div className="header-links">
-				<p>Home</p>
 				<p>Projects</p>
 				<p>Contact</p>
 			</div>
