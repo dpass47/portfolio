7baf304d13974271d50fce3e8e9505e7a0e39cdf
Dante Passalacqua <dante.l.pass@gmail.com>
2023-08-01 21:11:23 -0600

    added footer section

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import '../styles/Footer.css';
+
+function Footer() {
+	return (
+		<div className="footer">
+			<h2 className="contact-title">Contact</h2>
+			<div className="contact-links">
+				<a href="mailto:dante.l.pass@gmail.com">
+					dante.L.pass@gmail.com
+				</a>
+				<a href="https://linkedin.com/in/dpass47">LinkedIn</a>
+				<a href="https://github.com/dpass47">GitHub</a>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
