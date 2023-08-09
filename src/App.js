52e1092f33569be316af5f0272bab8a2c7e76799
Dante Passalacqua <dante.l.pass@gmail.com>
2023-08-09 16:17:41 -0600

    created separate data file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './Components/Header.js';
 import About from './Components/About.js';
 import Projects from './Components/Projects.js';
 import Footer from './Components/Footer.js';
+import data from './data.js';
 
 function App() {
 	return (
@@ -9,83 +10,7 @@ function App() {
 			<div className="container">
 				<Header />
 				<About />
-				<Projects
-					projects={[
-						{
-							image: '',
-							title: 'Celestial Dashboard',
-							summary:
-								'This project is based off the Momentum Dashboard chrome extension. I learned a tremendous amount in regards to working with APIs and building chrome extensions. The dashboard features weather, crypto price watch, quotes, and time widgets. All complete with a random space-themed background image taken from the Unsplash API.',
-							links: {
-								github: 'https://github.com/dpass47/celestial-dashboard',
-								liveSite:
-									'https://celestial-dashboard.devdante.com',
-							},
-							languages: ['HTML5', 'CSS3', 'JavaScript'],
-						},
-						{
-							image: '',
-							title: 'Tenzies Dice Game',
-							summary:
-								'This was my first app built with ReactJS, and I learned a great amount while building it. It is a dice game where users need to match the number of all ten die in order to win the game. The game tracks roll counts and time it takes to complete for high scores. When a user completes the game, confetti is shown to signify the win.',
-							links: {
-								github: 'https://github.com/dpass47/tenzies-game',
-								liveSite: 'https://tenzies.devdante.com',
-							},
-							languages: [
-								'HTML5',
-								'CSS3',
-								'JavaScript',
-								'ReactJS',
-							],
-						},
-						{
-							image: '',
-							title: 'Portfolio',
-							summary:
-								'This page right here! A responsive website built with ReactJS. The project content is passed in as props for auto-generating components for easy maintenance and additions.',
-							links: {
-								github: 'https://github.com/dpass47/portfolio',
-								liveSite: 'https://devdante.com',
-							},
-							languages: [
-								'HTML5',
-								'CSS3',
-								'Sass',
-								'JavaScript',
-								'ReactJS',
-							],
-						},
-						{
-							image: '',
-							title: 'Movie Watchlist',
-							summary:
-								"This was my first project working with APIs, it connects to the OMDb API to display movies with all necessary data. Using localStorage, users can save movies to their personal watchlist after searching for specified movies. The user's watchlist generates another call to the API to retrieve the data for the movies in their list.",
-							links: {
-								github: 'https://github.com/dpass47/movie-watchlist',
-								liveSite:
-									'https://movie-watchlist.devdante.com',
-							},
-							languages: ['HTML5', 'CSS3', 'JavaScript'],
-						},
-						{
-							image: '',
-							title: 'Digital Business Card',
-							summary:
-								'This is a react project that showcases my skills and interests in a business card format. It also shows the links to my socials to allow for easy access to my profiles for viewing.',
-							links: {
-								github: 'https://github.com/dpass47/digital-business-card',
-								liveSite: 'https://card.devdante.com',
-							},
-							languages: [
-								'HTML5',
-								'CSS3',
-								'JavaScript',
-								'ReactJS',
-							],
-						},
-					]}
-				/>
+				<Projects projects={data} />
 			</div>
 			<Footer />
 		</div>
---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = [
+	{
+		image: '',
+		title: 'Celestial Dashboard',
+		summary:
+			'This project is based off the Momentum Dashboard chrome extension. I learned a tremendous amount in regards to working with APIs and building chrome extensions. The dashboard features weather, crypto price watch, quotes, and time widgets. All complete with a random space-themed background image taken from the Unsplash API.',
+		links: {
+			github: 'https://github.com/dpass47/celestial-dashboard',
+			liveSite: 'https://celestial-dashboard.devdante.com',
+		},
+		languages: ['HTML5', 'CSS3', 'JavaScript'],
+	},
+	{
+		image: '',
+		title: 'Tenzies Dice Game',
+		summary:
+			'This was my first app built with ReactJS, and I learned a great amount while building it. It is a dice game where users need to match the number of all ten die in order to win the game. The game tracks roll counts and time it takes to complete for high scores. When a user completes the game, confetti is shown to signify the win.',
+		links: {
+			github: 'https://github.com/dpass47/tenzies-game',
+			liveSite: 'https://tenzies.devdante.com',
+		},
+		languages: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS'],
+	},
+	{
+		image: '',
+		title: 'Portfolio',
+		summary:
+			'This page right here! A responsive website built with ReactJS. The project content is passed in as props for auto-generating components for easy maintenance and additions.',
+		links: {
+			github: 'https://github.com/dpass47/portfolio',
+			liveSite: 'https://devdante.com',
+		},
+		languages: ['HTML5', 'CSS3', 'Sass', 'JavaScript', 'ReactJS'],
+	},
+	{
+		image: '',
+		title: 'Movie Watchlist',
+		summary:
+			"This was my first project working with APIs, it connects to the OMDb API to display movies with all necessary data. Using localStorage, users can save movies to their personal watchlist after searching for specified movies. The user's watchlist generates another call to the API to retrieve the data for the movies in their list.",
+		links: {
+			github: 'https://github.com/dpass47/movie-watchlist',
+			liveSite: 'https://movie-watchlist.devdante.com',
+		},
+		languages: ['HTML5', 'CSS3', 'JavaScript'],
+	},
+	{
+		image: '',
+		title: 'Digital Business Card',
+		summary:
+			'This is a react project that showcases my skills and interests in a business card format. It also shows the links to my socials to allow for easy access to my profiles for viewing.',
+		links: {
+			github: 'https://github.com/dpass47/digital-business-card',
+			liveSite: 'https://card.devdante.com',
+		},
+		languages: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS'],
+	},
+];
+
+export default data;
