3719050a08bfbbb2911596dccc3f1b4414ed18b2
Dante Passalacqua <dante.l.pass@gmail.com>
2023-08-09 19:28:50 -0600

    added images

--- src/data.js
+++ src/data.js
@@ -1,6 +1,12 @@
+import CelestialImage from './images/celestial-dashboard.png';
+import CardImage from './images/digital-business-card.png';
+import MovieImage from './images/movie-watchlist.png';
+import TenziesImage from './images/tenzies-game.png';
+import PortfolioImage from './images/portfolio.png';
+
 const data = [
 	{
-		image: '',
+		image: CelestialImage,
 		title: 'Celestial Dashboard',
 		summary:
 			'This project is based off the Momentum Dashboard chrome extension. I learned a tremendous amount in regards to working with APIs and building chrome extensions. The dashboard features weather, crypto price watch, quotes, and time widgets. All complete with a random space-themed background image taken from the Unsplash API.',
@@ -11,7 +17,7 @@ const data = [
 		languages: ['HTML5', 'CSS3', 'JavaScript'],
 	},
 	{
-		image: '',
+		image: TenziesImage,
 		title: 'Tenzies Dice Game',
 		summary:
 			'This was my first app built with ReactJS, and I learned a great amount while building it. It is a dice game where users need to match the number of all ten die in order to win the game. The game tracks roll counts and time it takes to complete for high scores. When a user completes the game, confetti is shown to signify the win.',
@@ -22,7 +28,7 @@ const data = [
 		languages: ['HTML5', 'CSS3', 'JavaScript', 'ReactJS'],
 	},
 	{
-		image: '',
+		image: PortfolioImage,
 		title: 'Portfolio',
 		summary:
 			'This page right here! A responsive website built with ReactJS. The project content is passed in as props for auto-generating components for easy maintenance and additions.',
@@ -33,18 +39,18 @@ const data = [
 		languages: ['HTML5', 'CSS3', 'Sass', 'JavaScript', 'ReactJS'],
 	},
 	{
-		image: '',
+		image: MovieImage,
 		title: 'Movie Watchlist',
 		summary:
 			"This was my first project working with APIs, it connects to the OMDb API to display movies with all necessary data. Using localStorage, users can save movies to their personal watchlist after searching for specified movies. The user's watchlist generates another call to the API to retrieve the data for the movies in their list.",
 		links: {
 			github: 'https://github.com/dpass47/movie-watchlist',
-			liveSite: 'https://movie-watchlist.devdante.com',
+			liveSite: 'http://movie-watchlist.devdante.com',
 		},
 		languages: ['HTML5', 'CSS3', 'JavaScript'],
 	},
 	{
-		image: '',
+		image: CardImage,
 		title: 'Digital Business Card',
 		summary:
 			'This is a react project that showcases my skills and interests in a business card format. It also shows the links to my socials to allow for easy access to my profiles for viewing.',
